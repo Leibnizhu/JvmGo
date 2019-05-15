77c7d7e9e2e57ddcf255cb695389f4b16c5924e4
Leibnizhu <leibnizhu@gmail.com>
2019-05-15 22:26:56 +0800

    增加slot入栈出栈方法

--- rtdata/operandStack.go
+++ rtdata/operandStack.go
@@ -37,7 +37,7 @@ func (self *OperandStack) PopFloat() float32 {
 	return math.Float32frombits(bits)
 }
 
-// long consumes two slots
+// long 需要2个 slot
 func (self *OperandStack) PushLong(val int64) {
 	self.slots[self.size].num = int32(val)
 	self.slots[self.size+1].num = int32(val >> 32)
@@ -50,7 +50,7 @@ func (self *OperandStack) PopLong() int64 {
 	return int64(high)<<32 | int64(low)
 }
 
-// double consumes two slots
+// double 需要2个 slot
 func (self *OperandStack) PushDouble(val float64) {
 	bits := math.Float64bits(val)
 	self.PushLong(int64(bits))
@@ -69,4 +69,13 @@ func (self *OperandStack) PopRef() *Object {
 	ref := self.slots[self.size].ref
 	self.slots[self.size].ref = nil //出栈之后，数组中对应设为nil，减少引用，方便GC
 	return ref
+}
+
+func (self *OperandStack) PushSlot(slot Slot) {
+	self.slots[self.size] = slot
+	self.size++
+}
+func (self *OperandStack) PopSlot() Slot {
+	self.size--
+	return self.slots[self.size]
 }
